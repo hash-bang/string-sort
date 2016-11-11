ce01dfe55c65f18be594d0353a2326eb7b2f2045
Matt Carter <m@ttcarter.com>
2016-11-11 15:51:49 +1000

    Initial version for testing

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var _ = require('lodash');
+
 var ss = module.exports = {
 	defaults: {
-		charOrder: 'abcdefghijklmnopqrstuvwxyz0123456789:/-_', // Character orders when comparing string positions (anything not here gets the ascii value)
+		charOrder: 'abcdefghijklmnopqrstuvwxyz0123456789:/-_', // Character orders when comparing string positions (anything not here gets run via fallback())
+		fallback: c => 999,
 	},
 
 	sort: function(arr, options) {
@@ -9,7 +12,22 @@ var ss = module.exports = {
 	},
 
 	compare: function(a, b, options) {
-		if (a == b) return 0;
-		return a > b ? 1 : -1;
+		var settings = _.defaults(options, this.defaults);
+
+		if (_.isNumber(a) && _.isNumber(b)) {
+			if (a == b) return 0;
+			return a > b ? 1 : -1;
+		} else if (_.isString(a) && _.isString(b)) {
+			for (var i = 0; i < Math.min(a.length, b.length); i++) {
+				var aChar = a.substr(i, 1), bChar = b.substr(i, 1);
+				var aVal = settings.charOrder.indexOf(aChar), bVal = settings.charOrder.indexOf(bChar);
+				if (aVal < 0) aVal = settings.fallback(aChar);
+				if (bVal < 0) bVal = settings.fallback(bChar);
+
+				if (aVal > bVal) return 1;
+				if (bVal < aVal) return -1;
+				// Equal values continue looping
+			}
+		}
 	},
 };
